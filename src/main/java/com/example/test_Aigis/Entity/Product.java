b2a9659d8d26bccd2b84962cdcf5dd3e37bb00f5
Mikhael-Wellman <mikhaelwellman423@gmail.com>
2025-06-18 23:11:24 +0700

    ate

--- src/main/java/com/example/test_Aigis/Entity/Product.java
+++ src/main/java/com/example/test_Aigis/Entity/Product.java
@@ -1,22 +1,22 @@
-package com.example.test_Aigis.Entity;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.*;
-
-@Entity
-@Setter
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder(toBuilder = true)
-public class Product {
-    //dds
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String name;
-}
+//package com.example.test_Aigis.Entity;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+//import lombok.*;
+//
+//@Entity
+//@Setter
+//@Getter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder(toBuilder = true)
+//public class Product {
+//    //dds
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String name;
+//}
--- src/main/java/com/example/test_Aigis/repository/ProductRepository.java
+++ src/main/java/com/example/test_Aigis/repository/ProductRepository.java
@@ -1,9 +1,9 @@
-package com.example.test_Aigis.repository;
-
-import com.example.test_Aigis.Entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
+//package com.example.test_Aigis.repository;
+//
+//import com.example.test_Aigis.Entity.Product;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface ProductRepository extends JpaRepository<Product, Long> {
+//}
