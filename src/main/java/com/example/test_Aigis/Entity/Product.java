0ff507771be765fa8415735da05b02672d14cf90
Mikhael-Wellman <mikhaelwellman423@gmail.com>
2025-06-18 22:23:32 +0700

    tes

---
+++ src/main/java/com/example/test_Aigis/Entity/Product.java
@@ -0,0 +1,22 @@
+package com.example.test_Aigis.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
